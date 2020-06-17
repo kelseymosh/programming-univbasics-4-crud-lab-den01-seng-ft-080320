89ff9ca37f8ae204a2557e1a89a83f97950baa93
Kelsey Oshiro <kelseymosh@gmail.com>
2020-06-17 21:57:36 +0000

    Automatically backed up by Learn

--- lib/array_crud.rb
+++ lib/array_crud.rb
@@ -7,11 +7,11 @@ def create_an_array
 end
 
 def add_element_to_end_of_array(array, element)
-  animal_crossing.push "Sherb"
+  puts animal_crossing.push "Sherb"
 end
 
 def add_element_to_start_of_array(array, element)
-  animal_crossing.unshift "Zucker"
+  puts animal_crossing.unshift "Zucker"
 end
 
 def remove_element_from_end_of_array(array)
