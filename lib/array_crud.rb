e1f8b97c2ce629d97efcd346b831e78b79a0709f
Kelsey Oshiro <kelseymosh@gmail.com>
2020-06-17 22:04:13 +0000

    Automatically backed up by Learn

--- lib/array_crud.rb
+++ lib/array_crud.rb
@@ -8,7 +8,7 @@ end
 
 def add_element_to_end_of_array(array, element)
   animal_crossing = ["Timmy","Tommy","Fang","Gladys"]
-  puts animal_crossing.push "arrays!"
+  puts animal_crossing.push "Sherb"
 end
 
 def add_element_to_start_of_array(array, element)
