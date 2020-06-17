aaa89ab2fa26c8d51c2d92724f340f19b450c795
Kelsey Oshiro <kelseymosh@gmail.com>
2020-06-17 22:13:04 +0000

    Automatically backed up by Learn

--- lib/array_crud.rb
+++ lib/array_crud.rb
@@ -19,7 +19,7 @@ def add_element_to_start_of_array(array, element)
 end
 
 def remove_element_from_end_of_array(array)
-  animal_crossing = ["Timmy","Tommy","Fang","Gladys"]
+  animal_crossing = ["wow","Tommy","Fang","arrays!"]
   animal_crossing.pop
 end
 
