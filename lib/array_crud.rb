c4da5a6fadcea7f4c4a84c997a1da07d98458882
Kelsey Oshiro <kelseymosh@gmail.com>
2020-06-17 22:16:08 +0000

    Automatically backed up by Learn

--- lib/array_crud.rb
+++ lib/array_crud.rb
@@ -29,7 +29,7 @@ def remove_element_from_start_of_array(array)
 end
 
 def retrieve_element_from_index(array, index_number)
-  animal_crossing = ["wow","Tommy","Fang",]
+  animal_crossing = ["wow","am","Fang",]
   animal_crossing[1]
 end
 
